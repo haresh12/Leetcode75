982640fe3342f38bb14c7a5fff034ce4567bea79
Haresh <lakhwaniharesh134@gmail.com>
2024-04-27 02:02:16 +0530

    Leetcode 75 astroid collision stack problem completed

--- stack/AsteroidCollision.js
+++ stack/AsteroidCollision.js
@@ -26,34 +26,41 @@ Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulti
  */
 
 
-// WIP
+/**
+ *  THIS ONE IS LIT
+ *  SEE WE HAVE HELL LOT OF CONDITIONS HERE IT TOOK MORE TIME THEN EXPECTED
+ *  EDGE CASES ARE LIKE HELL
+ * 
+ *  BUT FORGET ABOUT ABOVE ALL POINTS ONE THING THAT I MISSED IS LIKE
+ * 
+ *  IF AT TOP YOU HAVE -X AND THEN AT CURR YOU HAVE +Y THEN WE ARE NOT GOING TO DO ANYTHING JUST PUST BECAUSE - VALA ALREADY LEFT SIDE
+ *  CHLA GYA HE COLLITION POSSIBLE NHI HE TRY WITH [-2,-1,1,2]
+ */
 function  asteroidCollision(arr){
- let output = [];
-
- for(let i = 0 ; i < arr.length ; i++){
-      if(output.length === 0){
-        output.push(arr[i]);
-      }else {
-        let top = output[output.length - 1];
-        let curr = arr[i];
-        // both are in same direction nothing needed to be done
-        if((curr > 0  && top > 0) || (curr < 0  && top < 0)){
-            output.push(curr);
-        }else if(-top === curr){
-            output.pop();
-        }else if(-curr === top){
-            output.pop()
-        }
-        else if (top > 0 && curr < 0) {
-            // because current will colleps
-             continue;
-        }else if(top < 0 && curr > 0){
-             output.pop();
-             output.push(curr)
-        }
+ let output = [arr[0]];
+ 
+ for(let i  = 1 ; i < arr.length ; i++){
+   let curr = arr[i];
+    let top  =  output[output.length - 1];
+      
+       if(!top){
+        output.push(curr);
+        continue;
+       }
+       if((top > 0 && curr > 0) || (top < 0 && curr < 0) || (top < 0 || curr > 0)){
+        // this means both are in same direction;
+        output.push(curr);
+      }else if (((top > 0 && curr < 0) && (top === -curr)) || (top < 0 && curr > 0 && (-top === curr))){
+           output.pop();
+      }else if((curr < 0 && top > 0) && (-curr > top)) {
+          output.pop();
+          i--;  // IF YOU DON'T UNDERSTAND THIS LINE YOU ARE MISSING ENTIRE STUFF
+      }else if((top < 0 && curr > 0) && (-top > curr)){
+        continue;
       }
- }
- return output;
+   }
+
+ return output
 }
 
-console.log(asteroidCollision([10,2,-5]));
+console.log(asteroidCollision([-2,-2,1,-1]));
