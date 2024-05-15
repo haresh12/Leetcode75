d5ceaa8d2390e6c643f1c9cf692ec76072d95f29
Haresh <lakhwaniharesh134@gmail.com>
2024-05-15 22:53:37 +0530

    Leetcode valid squere

--- other/TeemoAttacking.js
+++ other/TeemoAttacking.js
@@ -44,3 +44,15 @@ var findPoisonedDuration = function(timeSeries, duration) {
     }
     return sumPoison
 };
+
+function validSquare(p1, p2, p3, p4) {
+  const getDistance = (x, y) => ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2);
+  const sides = new Set();
+  const points = [p1, p2, p3, p4];
+  for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+          if (i != j) sides.add(getDistance(points[i], points[j]));
+      }
+  }
+  return !sides.has(0) && sides.size == 2;
+}
